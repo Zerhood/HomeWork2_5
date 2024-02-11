0716395f75f2f13c44a47bdd95ed6480d22c6af6
Zerhood <Zerhood111@ya.ru>
2024-02-11 16:29:52 +0700

    init commit

---
+++ src/main/java/skypro/AppHomeWork25.java
@@ -0,0 +1,11 @@
+package skypro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppHomeWork25 {
+	public static void main(String[] args) {
+		SpringApplication.run(AppHomeWork25.class, args);
+	}
+}
